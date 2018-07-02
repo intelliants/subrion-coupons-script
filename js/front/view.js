3aae72c3b7482d70be54df2737d9b03fddcaf594
Daiyrbek Artelov <4unkur@mail.ru>
2018-07-02 10:28:18 +0600

    Report coupon functionality fixed. Code style improvements. Email templates adapted to new version

--- js/front/view.js
+++ js/front/view.js
@@ -1,22 +1,22 @@
 $(function () {
-    var modal = $('#report-coupon-modal');
+    var $modal = $('#report-coupon-modal');
+    var couponId;
+
     $('#js-cmd-report-coupon').on('click', function (e) {
         e.preventDefault();
         couponId = $(this).data('id');
-        modal.modal();
+        $modal.modal();
     });
 
     $('#report-coupon-form').on('submit', function (e) {
         e.preventDefault();
 
-        var comment = $('#report-coupon-comment');
-        var commentText = comment.val();
+        var $comment = $('#report-coupon-comment');
+        var url = intelli.config.packages.coupons.url + 'coupon/read.json';
 
-        $.post(intelli.config.packages.coupons.url + 'coupon/read.json', {
-            action: 'report', id: couponId, comments: commentText
-        }, function () {
-            comment.val('');
-            modal.modal('hide');
+        intelli.post(url, {action: 'report', id: couponId, comments: $comment.val()}, function () {
+            $comment.val('');
+            $modal.modal('hide');
             intelli.notifFloatBox({msg: _t('you_sent_report'), type: 'success', autohide: true});
         });
     });
@@ -29,28 +29,31 @@ $(function () {
     });
 
     // Picking tags
-    $('.couponItem .tag').on('click', function(e)
-    {
+    $('.couponItem .tag').on('click', function (e) {
         e.preventDefault();
 
         var tag = $.trim($(this).attr('href'));
         $('input[name="q"]').val(tag).closest('form').submit();
     });
 
-    $('.js-delete-coupon').on('click', function(e) {
+    $('.js-delete-coupon').on('click', function (e) {
         e.preventDefault();
         intelli.confirm(_t('delete_coupon_confirmation'), {url: $(this).attr('href')});
     });
 
-    $('.js-code-status').on('change', function(e) {
+    $('.js-code-status').on('change', function (e) {
         var $this = $(this).prop('disabled', true);
-        $.post(intelli.config.packages.coupons.url + 'coupon/read.json', {action: 'status', id: $this.data('id'), status: $this.val()}, function(response) {
+        intelli.post(intelli.config.packages.coupons.url + 'coupon/read.json', {
+            action: 'status',
+            id: $this.data('id'),
+            status: $this.val()
+        }, function (response) {
             $this.prop('disabled', false);
-            intelli.notifFloatBox({ msg: response.message, type: response.result ? 'success' : 'error', autohide: true});
+            intelli.notifFloatBox({msg: response.message, type: response.result ? 'success' : 'error', autohide: true});
         });
     });
 
-    $('.js-cmd-print-coupon').on('click', function() {
+    $('.js-cmd-print-coupon').on('click', function () {
         var win = window.open(window.location.href + '?print', '_blank');
         win.focus();
         // window.location.href = window.location.href + '?print';
